7fe5ed157e3015240c0db05f14945d1f405465b5
navadeep <164872889+navadeep2@users.noreply.github.com>
2025-02-28 21:03:24 +0530

    Add files via upload

---
+++ ExpenseManagement-PHP/connection.php
@@ -0,0 +1,14 @@
+<?php
+
+    $dsn = 'mysql:host=localhost:3307; dbname=expenseman';
+    $user = 'root';
+    $pass = '';
+
+    try {
+        $pdo = new PDO($dsn, $user, $pass);
+    }
+    catch(PDOException $e){
+        echo "Connection Error! ". $e->getMessage();
+    }
+?>
+
